4e6b5d494793f9e58468e870d17af95f0a0bb24f
Vernon-444 <mt.vernon44@gmail.com>
2022-05-20 10:53:34 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
