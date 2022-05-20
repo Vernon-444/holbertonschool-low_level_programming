8fb776ed8910750290e11c34b689c2f157279608
Vernon-444 <mt.vernon44@gmail.com>
2022-05-20 10:57:46 -0700

    testing 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
-/**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+/*
+ * File: 4-puts.c
+ * Auth: Brayden Vernon <4933@holbertonschool.com>
  */
 
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
