cb3e59e2e0f1a6d5a5434c5ef68da73915796f61
Vernon-444 <mt.vernon44@gmail.com>
2022-05-23 10:36:13 -0700

    testing 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char c = 'a';
+char C - 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+
+/*print in caps*/
+while (C <= 'Z')
+{
+putchar(C);
+C++;
+}
+putchar('\n');
+return (0);
+}
