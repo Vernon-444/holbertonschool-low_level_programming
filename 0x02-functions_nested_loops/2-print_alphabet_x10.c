7b734e12c02190cec679c934575453d9b8656749
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 11:04:06 -0700

    testing 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet followed by a new line 10x
+ * print_alphabet_x10 - prints alphabet followed by a new line 10x
  */
 
 void print_alphabet_x10(void)
 {
 char c;
 int i = 0;
-for (i = 0; i < 10; i ++)
+for (i = 0; i < 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
 {
