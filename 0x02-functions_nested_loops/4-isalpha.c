4956c8db1c7713fa7541f3aa5f2ecadc44d2ff7d
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 11:37:33 -0700

    tseting 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - checks string for any character
+ *
+ * @c: variable used in loop to compare if char is a character
+ *
+ * Return: 1 or 0 depending on case from 'c'
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z'
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
