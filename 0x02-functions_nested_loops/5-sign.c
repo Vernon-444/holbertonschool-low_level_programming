fe6d655188718a03562bc3040ee136f7d97871a7
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 12:04:40 -0700

    woah black betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,23 @@
 /**
  * print_sign - tests numbers and generates output
  *
+ * @n: determines num value
+ *
  * Return: returns value based on sign
  */
 
 int print_sign(int n)
 {
-  if (n < 0)
-    {
-      _putchar('-');
-      return (-1);
-    }
-  else if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-    }
-  _putchar('0');
-  return (0);
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+_putchar('0');
+return (0);
 }
