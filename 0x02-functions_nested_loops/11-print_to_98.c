c50da2ca43a4929b1f07d9eaec36ca38e75742d6
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 15:21:56 -0700

    testing 98 again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,20 @@
 
 void print_to_98(int n)
 {
-  if (n >= 98)
-    {
-      for (n = n; n > 98; n--)
-	{
-	  printf("%d, ", n);
-	}
-      printf("98");
-    }
+if (n >= 98)
+{
+for (n = n; n > 98; n--)
+{
+printf("%d, ", n);
+}
+printf("%d\n", n);
+}
 else
-  {
-    for (n = n; n < 98; n++)
-      {
-	printf("%d, ", n);
-      }
-    printf("98");
-  }
+{
+for (n = n; n < 98; n++)
+{
+printf("%d, ", n);
+}
+printf("%d\n", n);
+}
 }
