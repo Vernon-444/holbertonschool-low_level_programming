ddd2f23fcc2261566b3bd882ec97a2ec3321cca7
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 15:01:44 -0700

    testing 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints natural num from n to 98
+ *
+ * @n: var used to compair and count to 98
+ *
+ * Return: 0 on success
+ */
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+for (n = n; n > 98; n--)
+{
+printf("%d, ", n);
+}
+else
+{
+for (n = n; n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
+}
+}
