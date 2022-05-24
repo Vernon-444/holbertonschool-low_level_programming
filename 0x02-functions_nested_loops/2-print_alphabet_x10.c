c7b1bc6051256615516fd72be504f146b44c1f38
Vernon-444 <mt.vernon44@gmail.com>
2022-05-24 10:55:18 -0700

    tenfold

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,17 @@
 #include "main.h"
 /**
- * main - outputs alphabet using main.h
- *
- * Return: 0 if successful
+ * print_alphabet - prints alphabet followed by a new line 10x
  */
 
+void print_alphabet(void)
+{
+char c;
+for (i = 0; i < 10; i ++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
