b3961dd794284abf930abd9684629f46a60e5779
Vernon-444 <mt.vernon44@gmail.com>
2022-05-31 12:38:22 -0700

    testing 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0-9 except 2 & 4
+ *
+ * @i: variable for count and comparison
+ *
+ * return: 0 on success
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && c != 4)
+		{
+			_putchar(i);
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	_putchar(10);
+}
