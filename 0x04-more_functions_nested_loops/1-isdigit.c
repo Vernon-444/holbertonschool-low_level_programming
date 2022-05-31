5e2eda473c5e487ad1e3b71697c6511a84bd01f6
Vernon-444 <mt.vernon44@gmail.com>
2022-05-31 11:45:46 -0700

    testing 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - determines if it is a digit
+ *
+ * @c: digit used for comparision
+ *
+ * Return: 0 on success
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
