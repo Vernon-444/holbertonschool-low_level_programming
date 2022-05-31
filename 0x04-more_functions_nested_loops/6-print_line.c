926cfad26a5f8f42eb43a4254c237e4c2eca88a5
Vernon-444 <mt.vernon44@gmail.com>
2022-05-31 14:48:55 -0700

    testing 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * @n: number
+ *
+ * Return: 0 on success
+ */
+
+void print_line(int n)
+{
+	int n;
+	int i;
+
+	for (i = 0; i =< n; i++);
+	{
+		_putchar('_');
+	}
+	_putchar(10);
+}
