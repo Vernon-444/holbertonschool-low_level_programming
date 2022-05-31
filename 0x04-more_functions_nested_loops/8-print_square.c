5c652830f714cd77c8b804e23dda30062dddecf0
Vernon-444 <mt.vernon44@gmail.com>
2022-05-31 16:22:29 -0700

    testing 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,13 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 0; i <= size; i++)
 		{
-			for (i = 0; i <= size; i++)
+			for (j = 0; j <= size; j++)
 			{
 				_putchar(35);
 			}
