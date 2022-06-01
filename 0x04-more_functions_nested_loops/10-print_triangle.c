5b5b8b8a8e295750fb630314eae5e9d74e0aa424
Vernon-444 <mt.vernon44@gmail.com>
2022-05-31 17:27:51 -0700

    testing 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (dad = 1; dad < size; dad++)
+		for (dad = 0; dad <= size; dad++)
 		{
-			for (baby = 1; baby < size; baby++)
+			for (baby = 0; baby < size; baby++)
 			{
 				if (baby <= stage)
 				{
@@ -30,5 +30,9 @@ void print_triangle(int size)
 			stage--;
 			_putchar(10);
 		}
+	}
+	else
+	{
+		_putchar(10);
 	}
 }
