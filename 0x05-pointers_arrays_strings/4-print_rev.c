1133a79b4d9b23bca41202b8df4f37b87c2c3f0b
Vernon-444 <mt.vernon44@gmail.com>
2022-06-01 09:51:06 -0700

    betty

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string to stdout
+ *
+ * @s: increment char
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+
+	while (count--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar(10);
+}
