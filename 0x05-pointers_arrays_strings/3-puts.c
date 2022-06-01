62de1c6d412a093392273cd0f084541654797c74
Vernon-444 <mt.vernon44@gmail.com>
2022-06-01 09:24:22 -0700

    testing 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: increment char
+ *
+ * Return: 0 on success
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str++)
+	{
+		_putchar(*str);
+		i++;
+	}
+}
