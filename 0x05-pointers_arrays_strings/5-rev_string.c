38bc3711a1bbb7d8a5c3ba8a1d3da9177418a795
Vernon-444 <mt.vernon44@gmail.com>
2022-06-01 11:51:52 -0700

    testing 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - prints a string to stdout
+ *
+ * @s: increment char
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+
+	s--;
+
+	while (count > 0)
+	{
+		_putchar(*s);
+		s--;
+		count--;
+	}
+	_putchar(10);
+}
