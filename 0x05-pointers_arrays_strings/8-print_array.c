c09184bf49f6f4d362f5170242f32b82fc26aad9
Vernon-444 <mt.vernon44@gmail.com>
2022-06-02 10:17:16 -0700

    testing 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - prints array of n variables
+ *
+ * @n: iteration counter
+ * @a: array to modify
+ *
+ * Return: 0 on success
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf(a[n]);
+	}
+	printf("\n");
+}
