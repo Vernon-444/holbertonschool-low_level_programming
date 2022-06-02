36645dc88f47dcbad33aef36ba923dc478dd4ce5
Vernon-444 <mt.vernon44@gmail.com>
2022-06-02 10:06:26 -0700

    testing 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,13 @@
 
 void puts_half(char *str)
 {
-	int run = _strlen(str);
-	int i = (_strlen(str) / 2);
+	int run = _strlen(str) - 1;
+	int i = _strlen(str) / 2;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		i++;
+	}
 
 	while (i <= run)
 	{
