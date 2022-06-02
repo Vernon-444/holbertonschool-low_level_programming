894896c8e8efb7173c39c08834022700997f648b
Vernon-444 <mt.vernon44@gmail.com>
2022-06-02 11:56:24 -0700

    tseting 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,9 +17,9 @@ void print_array(int *a, int n)
 	if (n > 0)
 	{
 
-		for (i = 0; i < n - 1; i++)
+		for (i = 0; i < n; i++)
 		{
-			if (i != n - 2)
+			if (i != n - 1)
 			{
 				printf("%d, ", a[i]);
 			}
