9138b4e177fffb57a34c6d874534303e1ae27bd6
Vernon-444 <mt.vernon44@gmail.com>
2022-06-06 11:45:26 -0700

    tseting 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,16 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j = 0;
 
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
+	while (dest[i] != '\0')
 	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
