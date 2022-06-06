c2142027d099875841ae9efd8bbbd17547662252
Vernon-444 <mt.vernon44@gmail.com>
2022-06-06 14:07:04 -0700

    testing 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,28 +10,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0; j = 0;
 
-	while (s1[j] != '\0' && s2[j] != '0')
-	{
-		j++
-	}
+	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-			i = j - 1;
-		}
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 		i++;
-	}
-	if (s1[j] == s2[j])
-	{
-		return (0);
-	}
-	if (s1[j] != s2[j])
-	{
-		return (s1[j] - s2[j]
-	}
+	return (*(s1 + i) - *(s2 + i));
 }
