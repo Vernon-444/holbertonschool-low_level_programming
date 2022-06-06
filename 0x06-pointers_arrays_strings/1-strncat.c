34ea1e534cce7848f53eb3c585cb5b90412b39a9
Vernon-444 <mt.vernon44@gmail.com>
2022-06-06 12:34:19 -0700

    testing 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,10 +20,10 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	for (i = 0; i < n && src[i]; i++)
-		{
-			dest[dest_len + i] = src[i];
-		}
-	     dest[dest_len + i] = '\0';
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
 
-	     return dest;
+	return (dest);
 }
