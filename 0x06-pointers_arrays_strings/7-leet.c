00f5e3c7434b2f5ba0c6b989572b2d4e48b8a52a
Vernon-444 <mt.vernon44@gmail.com>
2022-06-06 19:08:00 -0700

    pushing 7 duggy-style

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,12 +2,26 @@
 /**
  * leet - encodes a string into 1337 (leet)
  *
- *
+ *@c: string to be adjusted
  *
  * Return: 0 0n success
  */
 
 char *leet(char *c)
 {
-	yo momma
+	char duggy[] = "aAeEoOtTlL4433007711";
+	int i, j;
+
+	for (j = 0; c[j] != '\0'; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (c[j] == duggy[i])
+				{
+					c[j] = duggy[i + 10];
+				}
+
+		}
+	}
+	return (c);
 }
