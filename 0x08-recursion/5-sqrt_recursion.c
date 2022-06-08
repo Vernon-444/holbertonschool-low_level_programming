4240c0f066fafeee397392f1a155682500e7a4c7
Vernon-444 <mt.vernon44@gmail.com>
2022-06-08 14:22:51 -0700

    testing 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,20 @@
 #include "main.h"
-#include "4-pow_recursion.c"
 
 /**
  * _sqrt_recursion - uses find_sqrt to get sqrt
- * find_sqrt - functin using recursion to calculate if sqrt of n is real
+ *
  * @n: int to be checked
  *
  * Return: sqrt in form of int
+ */
+
+/**
+ * find_sqrt - calculates sqrt for above funct
+ *
+ * @prev: increment var
+ * @find: n
+ *
+ * Return: sqrt int
  */
 
 int _sqrt_recursion(int n)
