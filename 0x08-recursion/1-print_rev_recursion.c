034762de7e4725daaeb4ed688e02981aba34e245
Vernon-444 <mt.vernon44@gmail.com>
2022-06-08 11:52:10 -0700

    testing 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _print_rev_recursion - prints string in rev
+*
+* @s: string to be modified
+*
+* Return: to end recursion
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
