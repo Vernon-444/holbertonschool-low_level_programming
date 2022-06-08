95f312a6dbe0477c49e896d914be866b4e2bffde
Vernon-444 <mt.vernon44@gmail.com>
2022-06-08 14:08:03 -0700

    tessting 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "4-pow_recursion.c"
+
+/**
+ * _sqrt_recursion - uses pow functoin to help calculate sqrt of int n
+ *
+ * @n: int to be checked
+ *
+ * Return: sqrt in form of int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(0, n));
+}
+
+int find_sqrt(int prev, int find)
+{
+	if (prev * prev == find)
+	{
+		return (prev);
+	}
+	if (prev * prev > find)
+	{
+		return (-1);
+	}
+	return (find_sqrt(prev + 1, find));
+}
