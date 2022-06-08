9281f53f279db41dd7c2d8ecedbe64d28db2d04d
Vernon-444 <mt.vernon44@gmail.com>
2022-06-08 10:01:54 -0700

    project setup

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * factorial - calculates the factorial of int n
+ * main - uses recursion with factorial to output
+ *
+ * @n: int to be modified
+ *
+ * Return: result of factorial (int)
+ */
+
