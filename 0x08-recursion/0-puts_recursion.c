61cef8191867b3ca3835dbbc4118975807994781
Vernon-444 <mt.vernon44@gmail.com>
2022-06-08 11:04:10 -0700

    testing 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ *
+ * @s: string to be copied
+ *
+ *
+ * Return: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(*s);
+}
