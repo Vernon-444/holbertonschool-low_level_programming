3955f59246a61facda0fa8935504a2759d24ba4e
Vernon-444 <mt.vernon44@gmail.com>
2022-06-17 13:04:38 -0700

    adjusted 0 and 1

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#indef _HEADER_
-#define _HEADER_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 #define SIZE 1024
 #endif
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,4 +1,4 @@
-#indef _HEADER_
-#define _HEADER_
+#indef PI_H
+#define PI_H
 #define PI 3.14159265359
 #endif
