f4dcec8b89b4a68aefc34548073aadf5cad27fc6
Vernon-444 <mt.vernon44@gmail.com>
2022-06-21 09:31:41 -0700

    setup proj and start 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates mem
+ *
+ * @b: unsigned int for mem alloc
+ *
+ * Return: 0 on success, 98 on fail
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return(p);
+}
