5438794e1478f4d6613fea70a9b782817b4d713c
Vernon-444 <mt.vernon44@gmail.com>
2022-06-28 14:14:03 -0700

    testing 2

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	print_strings(", ", 4, "Jay", "Django", "Duggy", "Connnnnnner");
+	return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings followed by new line
+ *
+ * @separator: to be printed between str"
+ * @n: num of elements
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list list;
+	char *a;
+
+	va_start(list, n);
+
+	if (n == 0)
+	{
+		return;
+	}
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+        for (i = 0; i < n - 1; i++)
+	{
+		a = va_arg(list, char *);
+		if (a)
+		{
+			printf("%s", a);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		printf("%s", separator);
+	}
+	a = va_arg(list, char *);
+	if (a)
+	{
+		printf("%s\n", a);
+	}
+	else
+	{
+		printf("%s\n", a);
+	}
+}
