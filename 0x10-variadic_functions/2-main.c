242bdea54244a9c02c327e508ef5ead41df6ed40
Vernon-444 <mt.vernon44@gmail.com>
2022-06-28 14:22:18 -0700

    tested for more edge cases

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -9,5 +9,8 @@ int main(void)
 {
 	print_strings(", ", 2, "Jay", "Django");
 	print_strings(", ", 4, "Jay", "Django", "Duggy", "Connnnnnner");
+	print_strings(NULL, 3, "Rock", "Paper", "Scissors");
+	print_strings(", yo momma, ", 3, "string 1", NULL, "string 3");
+	print_strings(", ", 0, "Jay", "Django");
 	return (0);
 }
