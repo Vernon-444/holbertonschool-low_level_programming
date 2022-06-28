02f47c00ee21c735a21a8e7698f669b66e304585
Vernon-444 <mt.vernon44@gmail.com>
2022-06-28 12:36:13 -0700

    tseting number 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - yup.
+ *
+ * @separator: - string to be printed betweeen each num
+ * @n: number of args given
+ *
+ * Return: noneya
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	if (n == 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d%s", va_arg(list, int), separator);
+	}
+	printf("%d\n", va_arg(list, int));
+
+	va_end(list);
+}
