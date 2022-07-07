98f1147af3826ede7c3fe6d4165dc66ca7c2da4d
Vernon-444 <mt.vernon44@gmail.com>
2022-07-07 10:08:08 -0700

    number 2 ready

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - prints num of elements of list_t list
+ *
+ * @h: pointer to list struct
+ *
+ * Return: Number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+
+}
