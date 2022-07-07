7db34e893ea08ec95a9871fe8fed56da44679577
Vernon-444 <mt.vernon44@gmail.com>
2022-07-07 10:32:44 -0700

    prep work for 2

---
+++ 0x12-singly_linked_lists/2-add_note.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ *
+ * @head: pointer to head
+ *
+ * @str: list of names provided
+ *
+ * Return: node and list
+ */
+
+list_t *add_nodes(list_t **head, const char *str)
+{
+	int i = 0;
+	char *tmp;
+	
+}
