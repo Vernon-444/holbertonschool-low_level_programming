33fd94d5e2a996e2e932a9fda87fa274cf98668c
Vernon-444 <mt.vernon44@gmail.com>
2022-07-12 08:49:31 -0700

    adjusted for edgecase NULL and betty notation

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,6 +15,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	newNode = malloc(sizeof(listint_t));
 
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
