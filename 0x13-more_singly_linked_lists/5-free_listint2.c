18588a1538b7493109ed41775a59a7fc6dd51143
Vernon-444 <mt.vernon44@gmail.com>
2022-07-12 09:05:39 -0700

    testing 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to null
+ *
+ * @head: pointer to head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	head = NULL;
+}
