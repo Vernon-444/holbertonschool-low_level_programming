c6a8902ca501c796b915546555feb0825e2f8748
Vernon-444 <mt.vernon44@gmail.com>
2022-07-12 15:59:55 -0700

    testing 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,4 +11,17 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	
+	unsigned int i = 0;
+
+
+        if (head == NULL)
+		return (0);
+
+	while (i != index)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+}
