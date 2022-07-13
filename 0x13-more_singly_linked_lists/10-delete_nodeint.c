68aa1cf5f97eaa03f509c4f0cc909e3dcc3f097e
Vernon-444 <mt.vernon44@gmail.com>
2022-07-12 18:18:48 -0700

    setup for q10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at index of list
+ *
+ * @head: pointer to head
+ * @index: index to be deleted
+ *
+ * Return: 1 on success, -1 if failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,5 +36,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	insert->next = p2;
 	insert->n = n;
 
-	return (insert);
+	return (insert->n);
 }
