4287523402809016acf278488059283606c5786d
Vernon-444 <mt.vernon44@gmail.com>
2022-07-12 17:49:39 -0700

    testing 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at given position
+ *
+ * @head: pointer to head
+ * @idx: index to insert node
+ * @n: value to be inserted
+ *
+ * Return: address of new node, NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *insert = malloc(sizeof(listint_t));
+	listint_t *p1, *p2;
+
+	if (head == NULL || insert == NULL)
+		return (NULL);
+
+	p1 = *head;
+	p2 = *head;
+
+	while (i != idx && head != NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next;
+		i++;
+	}
+
+	p2 = p2->next;
+
+	p1->next = insert;
+	insert->next = p2;
+	insert->n = n;
+
+	return (insert);
+}
