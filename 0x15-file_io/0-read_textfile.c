7c4a9c35bb1bab8fdea9d3a323f61b5ab714d807
Vernon-444 <mt.vernon44@gmail.com>
2022-07-18 09:29:14 -0700

    worked on 0-2 before checker release

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints it to POSIX standard output
+ *
+ * @filename: name of file to be read
+ * @letters: number of letters it should read and print
+ *
+ * Return: Num of letters read and printed, otherwise 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+
+	if (lenr != lenw)
+		return (0);
+
+	return (lenw);
+}
